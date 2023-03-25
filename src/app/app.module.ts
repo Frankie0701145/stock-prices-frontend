4f5a625ad3045b4cd35d0093a560abfb13d9eb49
Francis <francis@senga.co>
2023-03-25 17:24:17 +0300

    Create the stock-prices component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { StockPricesComponent } from './stock-prices/stock-prices.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StockPricesComponent
   ],
   imports: [
     BrowserModule,
